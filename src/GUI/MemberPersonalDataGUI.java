4e9051030d83cd6f496ca47ee294ec5d4cf2dfb6
lior203 <46168715+lior203@users.noreply.github.com>
2019-01-29 13:00:38 +0200

    lior

--- src/GUI/MemberPersonalDataGUI.java
+++ src/GUI/MemberPersonalDataGUI.java
@@ -24,102 +24,115 @@ import logic.RegistrationController;
 
 
 public class MemberPersonalDataGUI implements Initializable,GuiInterface{
-	 	@FXML
-	    private TextField txtFirst_Name;
+	@FXML
+	private TextField txtFirst_Name;
 
-	    @FXML
-	    private TextField txtLast_Name;
+	@FXML
+	private TextField txtLast_Name;
 
-	    @FXML
-	    private TextField txtID;
+	@FXML
+	private TextField txtID;
 
-	    @FXML
-	    private TextField txtPhone_Number;
+	@FXML
+	private TextField txtPhone_Number;
 
-	    @FXML
-	    private TextField txtEmail;
-	    @FXML
-	    private Button btnHistory;
-	    @FXML
-	    private TextField txtStatus;
-	    @FXML
-	    private Button btnSave;
-	    @FXML
-	    void updateMemberDetails(ActionEvent event) {
-		    	String ans=CommonController.checkInput(txtPhone_Number.getText(),txtEmail.getText(),txtID.getText());
-		    	if (ans.equals("EmailError")) {
-		    		showFailed("Wrong Email, please enter new parameters");
-				}
-		    	else if (ans.equals("PhoneError")) {
-		    		showFailed("Wrong phone number, please enter new parameters");
-				}
-		    	else {
-		    		RegistrationController.updateMemberDetails(txtID.getText(),txtPhone_Number.getText(), txtEmail.getText());
-		    		showSuccess("Details updated successfully");
-		    	}
-	    }
-	    @FXML
-	    void viewPersonalHistory(ActionEvent event) throws IOException {
-	    	//Load page of loan history
-	    	Parent parent=FXMLLoader.load(getClass().getResource("/GUI/HistoryOfLoanTableView.fxml"));
-	    	Scene scene=new Scene(parent);
-	    	Stage stage=new Stage();
-	    	stage.setScene(scene);
-	    	stage.setMaxHeight(631);
-	    	stage.setMinHeight(631);
-	    	stage.setMinWidth(920);
-	    	stage.setMaxWidth(920);
-	    	stage.show();
-	    	}
-	    Common.Member member;// object of Member details
-		@Override
-		public void showSuccess(String string) {
-			Alert alert=new Alert(AlertType.INFORMATION);
-			alert.setTitle("Success");
-			alert.setHeaderText(string);
-			alert.showAndWait();			
+	@FXML
+	private TextField txtEmail;
+	@FXML
+	private Button btnHistory;
+	@FXML
+	private TextField txtStatus;
+	@FXML
+	private Button btnSave;
+	@FXML
+	void updateMemberDetails(ActionEvent event) {
+		String ans=CommonController.checkInput(txtPhone_Number.getText(),txtEmail.getText(),txtID.getText());
+		if (ans.equals("EmailError")) {
+			showFailed("Wrong Email, please enter new parameters");
 		}
+		else if (ans.equals("PhoneError")) {
+			showFailed("Wrong phone number, please enter new parameters");
+		}
+		else {
+			RegistrationController.updateMemberDetails(txtID.getText(),txtPhone_Number.getText(), txtEmail.getText());
+			showSuccess("Details updated successfully");
+		}
+		txtEmail.setEditable(false);
+		txtPhone_Number.setEditable(false);
+	}
 
-		@Override
-		public void display(Object obj) {
-			ArrayList<String>memberData=(ArrayList<String>) obj;
-			member=new Common.Member(memberData.get(1), memberData.get(2), memberData.get(3), memberData.get(4), memberData.get(5), memberData.get(6), memberData.get(7), memberData.get(8), memberData.get(9), memberData.get(10), memberData.get(11), memberData.get(12));
-			setCardMember(memberData);
-			setEditableMember();
-		}
+	@FXML
+	void emailEdit(ActionEvent event) {
+		txtEmail.setEditable(true);
+	}
 
-		@Override
-		public void showFailed(String message) {
-			Alert alert=new Alert(AlertType.ERROR);
-			alert.setTitle("Error");
-			alert.setHeaderText(message);
-			alert.showAndWait();			
-		}
+	@FXML
+	void phoneEdit(ActionEvent event) {
+		txtPhone_Number.setEditable(true);
+	}
 
-		@Override
-		public void initialize(URL arg0, ResourceBundle arg1) {
-			Main.client.clientUI=this;
-			init();
-		}
-		private void init() {
-			RegistrationController.searchMember(Client.arrayUser.get(0));
-		}
-		private void setEditableMember() {
-			txtFirst_Name.setEditable(false);
-			txtLast_Name.setEditable(false);
-			txtPhone_Number.setEditable(true);
-			txtEmail.setEditable(true);
-			txtStatus.setEditable(false);
-		}
-		private void setCardMember(ArrayList<String> memberData) {
-			txtID.setText(memberData.get(1));
-			txtFirst_Name.setText(memberData.get(5));
-			txtLast_Name.setText(memberData.get(6));
-			txtPhone_Number.setText(memberData.get(2));
-			txtEmail.setText(memberData.get(3));
-			txtStatus.setText(memberData.get(7));
-		}
-/*		private void setFields(Boolean cond) {
+	@FXML
+	void viewPersonalHistory(ActionEvent event) throws IOException {
+		//Load page of loan history
+		Parent parent=FXMLLoader.load(getClass().getResource("/GUI/HistoryOfLoanTableView.fxml"));
+		Scene scene=new Scene(parent);
+		Stage stage=new Stage();
+		stage.setScene(scene);
+		stage.setMaxHeight(631);
+		stage.setMinHeight(631);
+		stage.setMinWidth(920);
+		stage.setMaxWidth(920);
+		stage.show();
+	}
+	Common.Member member;// object of Member details
+	@Override
+	public void showSuccess(String string) {
+		Alert alert=new Alert(AlertType.INFORMATION);
+		alert.setTitle("Success");
+		alert.setHeaderText(string);
+		alert.showAndWait();			
+	}
+
+	@Override
+	public void display(Object obj) {
+		ArrayList<String>memberData=(ArrayList<String>) obj;
+		member=new Common.Member(memberData.get(1), memberData.get(2), memberData.get(3), memberData.get(4), memberData.get(5), memberData.get(6), memberData.get(7), memberData.get(8), memberData.get(9), memberData.get(10), memberData.get(11), memberData.get(12));
+		setCardMember(memberData);
+		setEditableMember();
+	}
+
+	@Override
+	public void showFailed(String message) {
+		Alert alert=new Alert(AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText(message);
+		alert.showAndWait();			
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Main.client.clientUI=this;
+		init();
+	}
+	private void init() {
+		RegistrationController.searchMember(Client.arrayUser.get(0));
+	}
+	private void setEditableMember() {
+		txtFirst_Name.setEditable(false);
+		txtLast_Name.setEditable(false);
+		txtPhone_Number.setEditable(true);
+		txtEmail.setEditable(true);
+		txtStatus.setEditable(false);
+	}
+	private void setCardMember(ArrayList<String> memberData) {
+		txtID.setText(memberData.get(1));
+		txtFirst_Name.setText(memberData.get(5));
+		txtLast_Name.setText(memberData.get(6));
+		txtPhone_Number.setText(memberData.get(2));
+		txtEmail.setText(memberData.get(3));
+		txtStatus.setText(memberData.get(7));
+	}
+	/*		private void setFields(Boolean cond) {
 			txtFirst_Name.setDisable(cond);
 			txtLast_Name.setDisable(cond);
 			txtPhone_Number.setDisable(cond);
@@ -129,9 +142,9 @@ public class MemberPersonalDataGUI implements Initializable,GuiInterface{
 		//	btnLates_Lostbook.setDisable(cond);
 			btnSave.setDisable(cond);
 		}*/
-		@Override
-		public void freshStart() {
-			// TODO Auto-generated method stub
-			
-		}
+	@Override
+	public void freshStart() {
+		// TODO Auto-generated method stub
+
+	}
 }
